68d83e5aa068f0cedbb536c58ad904f19b440b8f
asapupravallika <asapupravallika@gmail.com>
2026-07-02 15:07:01 +0530

    feat: Comparable vs Comparator Custom Objects with Custom Sorting Order

---
+++ src/com/codegnan/CollectionFramework/PersonComparator.java
@@ -0,0 +1,13 @@
+package com.codegnan.CollectionFramework;
+
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+
+	@Override
+	public int compare(Person o1, Person o2) {
+		//return o1.getName().compareTo(o2.getName());
+		return -Integer.compare(o1.getId(), o2.getId());
+	}
+
+}
