36cfcb4dec223f24fb40d79efa7c34c0cc676423
asapupravallika <asapupravallika@gmail.com>
2026-07-02 15:30:24 +0530

    feat: Comparable and Comparator

---
+++ src/com/codegnan/CollectionFramework/TreeSetPerson.java
@@ -0,0 +1,18 @@
+package com.codegnan.CollectionFramework;
+
+import java.util.TreeSet;
+
+public class TreeSetPerson {
+
+	public static void main(String[] args) {
+		TreeSet<Person> t = new TreeSet<>(new PersonComparator());
+		t.add(new Person(102, "Charlie", "charlie@example.com"));
+		t.add(new Person(101, "Alice", "alice@example.com"));
+		t.add(new Person(103, "Bob", "bob@example.com"));
+		t.add(new Person(104, "David", "david@example.com"));
+		t.add(new Person(105, "Eve", "eve@example.com"));
+		t.add(new Person(106, "Alice", "alice@example.com"));
+		System.out.println(t);
+	}
+
+}
